9263251cd2fbd602061a947a1d321e9b8d923693
sharath <152282735+Sharath8i@users.noreply.github.com>
2025-05-17 22:29:49 +0530

    Update P4.java

--- LAB2/src/lab2/P4.java
+++ LAB2/src/lab2/P4.java
@@ -1,3 +1,12 @@
+/*
+Write a Java program to check whether a map contains Key-Values mappings (empty) or not
+ after adding all the  following elements into Hashmap and after removing all the elements from
+ the Hashmap. 
+ (1, "Red"),(2, "Green"), (3, "Black"),  (4, "White"),(5, "Blue")
+	*/
+
+
+
 package lab2;
 
 import java.util.HashMap;
