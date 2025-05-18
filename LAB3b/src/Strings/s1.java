9cdbb9ab475eedd637774cd64525596ecc6213d5
sharath <152282735+Sharath8i@users.noreply.github.com>
2025-05-18 09:01:08 +0530

    Update s1.java

--- LAB3b/src/Strings/s1.java
+++ LAB3b/src/Strings/s1.java
@@ -1,3 +1,12 @@
+/*Given a string, write a method to determine if it is a palindrome (reads the same 
+forward and backward), ignoring case and non-alphanumeric characters. Input: "A man, a plan, 
+a canal: Panama"
+Output: true Input: "race a car"
+ Output: false */
+
+
+
+
 package Strings;
 
 public class s1 {
