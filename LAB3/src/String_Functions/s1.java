31f6751bb39576b484c60c971bf7d13ba636d6b9
sharath <152282735+Sharath8i@users.noreply.github.com>
2025-05-18 08:51:10 +0530

    Update s1.java

--- LAB3/src/String_Functions/s1.java
+++ LAB3/src/String_Functions/s1.java
@@ -1,3 +1,12 @@
+/*g
+ 1. Question: Given a string, write a method to determine if it is a palindrome (reads the same 
+forward and backward), ignoring case and non-alphanumeric characters. Input: "A man, a plan, 
+a canal: Panama"
+Output: true Input: "race a car"
+ Output: false */
+
+
+
 package String_Functions;
 
 public class s1 {
